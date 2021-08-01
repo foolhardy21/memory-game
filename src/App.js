5adb1161243344c22e6e9ec54f98d61b2b2bd6d6
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-08-01 15:21:16 +0530

    minimum viable project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import data from './listData'
 
-function App() {
+const App = () => {
+ const [currentScore, setCurrentScore] = useState(0)
+ const [totalScore, setTotalScore] = useState(0)
+ const [list, setList] = useState(data)
+ 
+ const shuffleList = (clickedlist) => {
+   const newlist = [...clickedlist]
+   for( let i = newlist.length-1; i >= 1; i--) {
+    let j = Math.floor(Math.random()*i)+0
+    let sub = newlist[j]
+    newlist[j] = newlist[i]
+    newlist[i] = sub
+   }
+   setList(newlist)
+   
+ }
+ const updateClickStatus = (id) => {
+   
+  let newlist = [...list]
+  newlist = newlist.map((current) => {
+    if (current.id === id) {
+      return {...current, clicked: true}
+    } else
+    return current
+  })
+  shuffleList(newlist)
+ }
+
+ const handleClick = (id, clicked) => {
+  
+    if (!clicked) {
+      updateClickStatus(id)  
+      setCurrentScore(currentScore + 1)
+    } else {
+      setTotalScore(currentScore) 
+      setCurrentScore(0)
+      setList(data)
+    }
+   
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='content'>
+      <div>
+        Max Score - {totalScore}
+      </div>
+      <div>
+        Current Score - {currentScore}
+      </div>
+      <ul>
+        {
+          list.map(item => {
+            const { id, name, clicked } = item
+            return <li key={id} 
+            onClick={() => handleClick(id, clicked)}
+            >
+              {name}
+            </li>
+          })
+        }
+      </ul>
     </div>
+       
   );
 }
 
---
+++ src/listData.js
@@ -0,0 +1,55 @@
+const data = [
+    {
+        id: 0,
+        name: 'A',
+        clicked: false,
+    },
+    {
+        id: 1,
+        name: 'B',
+        clicked: false,
+    },
+    {
+        id: 2,
+        name: 'C',
+        clicked: false,
+    },
+    {
+        id: 3,
+        name: 'D',
+        clicked: false,
+    },
+    {
+        id: 4,
+        name: 'E',
+        clicked: false,
+    },
+    {
+        id: 5,
+        name: 'F',
+        clicked: false,
+    },
+    {
+        id: 6,
+        name: 'G',
+        clicked: false,
+    },
+    {
+        id: 7,
+        name: 'H',
+        clicked: false,
+    },
+    {
+        id: 8,
+        name: 'I',
+        clicked: false,
+    },
+    {
+        id: 9,
+        name: 'J',
+        clicked: false,
+    },
+
+]
+
+export default data
